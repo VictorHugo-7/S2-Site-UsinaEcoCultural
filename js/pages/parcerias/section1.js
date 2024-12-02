bd22668e8ee473f140879c4fd9b29ba0f4864d05
VHP77 <156820479+VictorHugo-7@users.noreply.github.com>
2024-12-02 14:35:28 -0300

    atualização página parcerias

--- js/pages/parcerias/section1.js
+++ js/pages/parcerias/section1.js
@@ -3,6 +3,67 @@ fetch('../../../html/pages/parcerias/section1.html')
     .then(data => {
         document.getElementById('my-parcerias-s1-importacao').innerHTML = data;
 
+        const connections = [
+            { 
+                name: 'Cades Ipiranga', 
+                description: 'Conselho Regional de Meio Ambiente, Desenvolvimento Sustentável e Cultura de Paz',
+                image: '../../../midias/img/pages/parcerias/conexaoCadesIpiranga.png'
+            },
+            { 
+                name: 'Cades Vila Mariana', 
+                description: 'Voltado para ações ambientais e sociais na região da Vila Mariana',
+                image: '../../../midias/img/pages/parcerias/conexaoCadesVilaMariana.jpg'
+            },
+            { 
+                name: 'Casa Amarela', 
+                description: 'Espaço cultural e comunitário que promove atividades de arte, cultura e sustentabilidade',
+                image: '../../../midias/img/pages/parcerias/conexaoCasaAmarela.png'
+            },
+            { 
+                name: 'Muda Mooca', 
+                description: 'Sustentabilidade e envolvimento local',
+                image: '../../../midias/img/pages/parcerias/conexaoMudaMooca.png'
+            },
+            { 
+                name: 'Missão Ambiental', 
+                description: 'Organização que atua em educação e conscientização ambiental',
+                image: '../../../midias/img/pages/parcerias/conexaoMissaoAmbiental.jpg'
+            },
+            { 
+                name: 'Muda Ipiranga', 
+                description: 'Coletivo comunitário que realiza ações de plantio e educação ambiental',
+                image: '../../../midias/img/pages/parcerias/conexaoMudaIpiranga.jpg'
+            },
+            { 
+                name: 'Casa Das Caldeiras', 
+                description: 'Centro cultural e histórico em São Paulo',
+                image: '../../../midias/img/pages/parcerias/conexaoCasaDasCaldeiras.png'
+            },
+            { 
+                name: 'Museu do Ipiranga', 
+                description: 'Instituição histórica e cultural dedicada à preservação e divulgação da história do Brasil',
+                image: '../../../midias/img/pages/parcerias/conexaoMuseuDoIpiranga.png'
+            }
+        ];
 
+        function createGrid(container, data) {
+            const grid = document.getElementById(container);
+            data.forEach((item, index) => {
+                const card = document.createElement('div');
+                card.className = 'my-parcerias-s1-partner-card';
+                card.innerHTML = `
+                    <img src="${item.image}" alt="${item.name}">
+                `;
+                card.addEventListener('click', () => {
+                    const modal = new bootstrap.Modal(document.getElementById('partnerModal'));
+                    document.getElementById('modalTitle').textContent = item.name;
+                    document.getElementById('modalDescription').textContent = item.description;
+                    modal.show();
+                });
+                grid.appendChild(card);
+            });
+        }
+
+        createGrid('connectionsGrid', connections);
     })
     .catch(error => console.error('Erro ao carregar a página:', error));
--- js/pages/parcerias/section2.js
+++ js/pages/parcerias/section2.js
@@ -3,6 +3,68 @@ fetch('../../../html/pages/parcerias/section2.html')
     .then(data => {
         document.getElementById('my-parcerias-s2-importacao').innerHTML = data;
 
+        const partners = [
+            { 
+                name: 'Instituto Mauá de Tecnologia', 
+                description: 'Centro de referência em tecnologia, engenharia, design e administração',
+                image: '../../../midias/img/pages/parcerias/parceiroInstitutoMauaDeTecnologia.jpg'
+            },
+            { 
+                name: 'Centro Universitário São Camilo', 
+                description: 'Instituição de ensino superior com destaque em áreas da saúde',
+                image: '../../../midias/img/pages/parcerias/parceiroCentroUniversitarioSaoCamilo.png'
+            },
+            { 
+                name: 'Etec Getúlio Vargas', 
+                description: 'Escola Técnica Estadual de São Paulo de ensino técnico e profissionalizante.',
+                image: '../../../midias/img/pages/parcerias/parceiroEtecGetulioVargas.jpg'
+            },
+            { 
+                name: 'FAUUSP', 
+                description: 'Renomada escola de arquitetura e urbanismo',
+                image: '../../../midias/img/pages/parcerias/parceiroFauusp.png'
+            },
+            { 
+                name: 'USCS', 
+                description: 'Instituição pública de ensino superior que oferece cursos em diversas áreas',
+                image: '../../../midias/img/pages/parcerias/parceiroUscs.png'
+            },
+            { 
+                name: 'Escola Nacional Paulo Freire', 
+                description: 'Escola de formação baseada na pedagogia crítica de Paulo Freire',
+                image: '../../../midias/img/pages/parcerias/parceiroEscolaNacionalPauloFreire.jpg'
+            },
+            { 
+                name: 'Escola Da Cidade', 
+                description: 'Faculdade de arquitetura e urbanismo',
+                image: '../../../midias/img/pages/parcerias/parceiroEscolaDaCidade.png'
+            },
+            { 
+                name: 'Escoteiros do Brasil', 
+                description: 'Movimento educacional que promove valores de cidadania, liderança e sustentabilidade',
+                image: '../../../midias/img/pages/parcerias/parceiroEscoteirosDoBrasil.png'
+            }
+        ];
 
+
+        function createGrid(container, data) {
+            const grid = document.getElementById(container);
+            data.forEach((item, index) => {
+                const card = document.createElement('div');
+                card.className = 'my-parcerias-s2-partner-card';
+                card.innerHTML = `
+                    <img src="${item.image}" alt="${item.name}">
+                `;
+                card.addEventListener('click', () => {
+                    const modal = new bootstrap.Modal(document.getElementById('partnerModal'));
+                    document.getElementById('modalTitle').textContent = item.name;
+                    document.getElementById('modalDescription').textContent = item.description;
+                    modal.show();
+                });
+                grid.appendChild(card);
+            });
+        }
+
+        createGrid('partnersGrid', partners);
     })
     .catch(error => console.error('Erro ao carregar a página:', error));
